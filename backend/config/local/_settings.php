0317285fe5d4a1fe04839b6732d3ee909be2010d
Abhimanyu Saharan <abhi1693@users.noreply.github.com>
2015-02-01 00:40:31 +0530

    Update _settings.php

--- backend/config/local/_settings.php
+++ backend/config/local/_settings.php
@@ -1,14 +1,1 @@
 <?php
-	return array(
-		'components' =>
-			array(
-				'db' =>
-					array(
-						'class'    => 'yii\\db\\Connection',
-						'dsn'      => 'mysql:host=localhost;dbname=starter-kit',
-						'username' => 'abhimanyu',
-						'password' => 'kp171095',
-						'charset'  => 'utf8',
-					),
-			),
-	); ?>
