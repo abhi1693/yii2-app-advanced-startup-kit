bf23b44050a964de74fe286d4cdec26e4b3011b7
Abhimanyu Saharan <abhi16_93@yahoo.in>
2015-02-01 23:22:30 +0530

    changed || with &&

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -30,7 +30,7 @@
 		public function actionIndex()
 		{
 			// Checks if the application has been installed successfully
-			if (Yii::$app->config->get(Enum::APP_SECRET) != '' || Yii::$app->params['installed'] === TRUE)
+			if (Yii::$app->config->get(Enum::APP_SECRET) != '' && Yii::$app->params['installed'] === TRUE)
 				return $this->render('index');
 			else
 				return $this->redirect(Yii::$app->urlManager->createUrl('//installer/install/index'));
