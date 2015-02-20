aa455e5ff41a3da751ce61b5a82047d401c38398
Abhimanyu Saharan <abhi16_93@yahoo.in>
2015-02-20 13:25:09 +0530

    [+] common function

---
+++ backend/helper/Common.php
@@ -0,0 +1,25 @@
+<?php
+	/**
+	 * Created by PhpStorm.
+	 * User: Abhimanyu
+	 * Date: 20-02-2015
+	 * Time: 13:12
+	 */
+
+	namespace backend\helper;
+
+	use yii\helpers\Html;
+
+	class Common
+	{
+
+		public static function getVersion()
+		{
+			return 'v0.0.2-dev';
+		}
+
+		public static function getCreator()
+		{
+			return Html::mailto('Abhimanyu Saharan', 'abhimanyu@teamvulcans.com');
+		}
+	}
