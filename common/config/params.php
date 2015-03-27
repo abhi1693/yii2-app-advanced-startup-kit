a517497618a3fb92854d60fea0816a1b76a9f821
Abhimanyu Saharan <abhimanyu@teamvulcans.com>
2015-03-28 03:00:30 +0530

    [+] Dynamic Params File

--- common/config/params.php
+++ common/config/params.php
@@ -3,5 +3,6 @@
 use abhimanyu\installer\helpers\enums\Configuration as Enum;
 
 return [
-	Enum::CONFIG_FILE => dirname(__FILE__) . '/main-local.php'
-	];
+	Enum::CONFIG_FILE => dirname(__FILE__) . '/main-local.php',
+	Enum::PARAMS_FILE => dirname(__FILE__) . '/params-local.php'
+];
