57c55ceed6b57704a150a66bc0ccc629784e60d9
Abhimanyu Saharan <abhimanyu@teamvulcans.com>
2015-04-16 11:50:35 +0530

    Small Bug Fixes

--- backend/assets_b/AppAsset.php
+++ backend/assets_b/AppAsset.php
@@ -20,8 +20,7 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web/assets_b';
     public $css = [
         'css/site.css',
-        'font-awesome/css/font-awesome.min.css',
-        'css/AdminLTE.css'
+        'font-awesome/css/font-awesome.min.css'
     ];
     public $js = [
     ];
